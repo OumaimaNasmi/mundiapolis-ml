00704c0695148bc35f12f567570404afb32f8a54
OumaimaNasmi <80533463+OumaimaNasmi@users.noreply.github.com>
2021-03-31 10:03:10 +0100

    Update 1-neuron.py

--- 0x01-classification/1-neuron.py
+++ 0x01-classification/1-neuron.py
@@ -1,17 +1,49 @@
 #!/usr/bin/env python3
-
+"""
+Module to create a neuron
+"""
 import numpy as np
 
-Neuron = __import__('1-neuron').Neuron
 
-lib_train = np.load('../data/Binary_Train.npz')
-X_3D, Y = lib_train['X'], lib_train['Y']
-X = X_3D.reshape((X_3D.shape[0], -1)).T
+class Neuron:
+    """
+    A class that defines a single neuron
+    """
 
-np.random.seed(0)
-neuron = Neuron(X.shape[0])
-print(neuron.W)
-print(neuron.b)
-print(neuron.A)
-neuron.A = 10
-print(neuron.A)
+    def __init__(self, nx):
+        """
+        class constructor
+        :param nx: is the number of input features to the neuron
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """
+        getter function for W
+        :return: weight vector neuron
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """
+        getter function for b
+        :return: bias for neuron
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """
+        getter function for W
+        :return: activated output of the neuron
+        """
+        return self.__A
